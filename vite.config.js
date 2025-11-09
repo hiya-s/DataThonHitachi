0976fa9a3034a5009a91d28612a94153e8d372ca
hiya-sha <hiya.sharma@tamu.edu>
2025-11-08 20:46:06 -0600

    better ui- cute

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Make env variables available in the app
+  }
 })
